a30f7c981e7c9c4f372f23273f997597174c2b60
Enrique Marques <86737659+Enrienriq@users.noreply.github.com>
2021-07-07 16:18:18 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+function direcionar(){
+    open("https://www.geekno.com/pt/pokedex/charmander")
+}
+function direcionars(){
+    open("https://www.geekno.com/en/pokedex/squirtle")
+}
+function direcionart(){
+    open("https://www.geekno.com/pt/pokedex/bulbasaur")
+}        
